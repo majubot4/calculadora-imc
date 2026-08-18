f9a57eba14f317cfcbb520347cabbe072cb6ad04
naclara09 <anna.holanda@escolar.ifrn.edu.br>
2026-08-18 17:33:45 -0300

    Sobre

--- CalculadoraIMC/src/App.jsx
+++ CalculadoraIMC/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import PaginaCalculo from "./components/PaginaCalculo";
+import Sobre from "./components/Sobre";
 
 function App() {
-  return (
-    <div className="App">
-      <PaginaCalculo />
-    </div>
-  );
+    return (
+        <div className="App">
+            <PaginaCalculo />
+            <Sobre />
+        </div>
+    );
 }
 
 export default App;
