f065628566dbedcdeac3acb9b47977bdc14fc0cb
naclara09 <anna.holanda@escolar.ifrn.edu.br>
2026-08-19 21:06:04 -0300

    Finalizando

--- CalculadoraIMC/CalculadoraIMC/src/App.jsx
+++ CalculadoraIMC/CalculadoraIMC/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import PaginaCalculo from "./components/PaginaCalculo";
+import Sobre from "./components/Sobre";
 
 function App() {
     return (
         <div className="App">
             <PaginaCalculo />
+            <Sobre />
         </div>
     );
 }
--- CalculadoraIMC/src/App.jsx
+++ CalculadoraIMC/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import PaginaCalculo from "./components/PaginaCalculo";
-import Sobre from "./components/Sobre";
 
 function App() {
     return (
         <div className="App">
             <PaginaCalculo />
-            <Sobre />
         </div>
     );
 }
